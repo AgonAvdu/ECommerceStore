11a34d8df7bf55e45c36d329182a022b69002d37
TigranGashi <93322726+TigranGashi@users.noreply.github.com>
2022-06-19 21:39:27 +0200

    Vazhdim.

---
+++ ECommerce/DTOs/CartItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.DTOs
+{
+    public class CartItemDTO
+    {
+        public int productId { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public string imgUrl { get; set; }
+        public double sale { get; set; }
+        public int categoryId { get; set; }
+        public int quantity { get; set; }
+    }
+}
---
+++ ECommerce/Model/Cart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Model
+{
+    public class Cart
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int id { get; set; }
+        public string buyerId { get; set; }
+        public List<CartItem> items { get; set; } = new List<CartItem>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            if (items.All(item => item.productId != product.id))
+            {
+                items.Add(new CartItem { Product = product, quantity = quantity });
+            }
+            var existingItem = items.FirstOrDefault(item => item.productId == product.id);
+            if (existingItem != null) existingItem.quantity += quantity;
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            var item = items.FirstOrDefault(item => item.productId == productId);
+            if (item == null) return;
+            item.quantity -= quantity;
+            if (item.quantity == 0) items.Remove(item);
+        }
+
+    }
+}
+
